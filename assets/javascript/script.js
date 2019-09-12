c4761d138a1838437866687ed7f137573407f42a
Ckopei <cameron@kopels.com>
2019-09-12 14:10:17 -0400

    added code to clear article div on new searches

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
     $("#search").on("click", function (event) {
+        $("#articles").empty()
         event.preventDefault()
         // console.log($(this).val())
         console.log("You clicked me")
