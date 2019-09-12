325828e07ca92d3f05f202bcc5610b47fc038885
Ckopei <cameron@kopels.com>
2019-09-12 13:55:03 -0400

    finished data end.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,36 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-$("#search").on("click", function(event){
-    event.preventDefault()
-    // console.log($(this).val())
-    console.log("You clicked me")
-})
+    $("#search").on("click", function (event) {
+        event.preventDefault()
+        // console.log($(this).val())
+        console.log("You clicked me")
+        var articleCounter = 0;
+        var startYear = "begin_date=" + $("#start-year").val() + "0101"
+        var endYear = "&end_date=" + $("#end-year").val() + "0101"
+        var searchTerm = $("search-term").val()
+        var numArticles = $("#records-num").val()
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"+startYear+endYear+"+&q="+ searchTerm+"&sort=newest&api-key=SGPpcmFpXFAh90XV4TR2KvprCYlJA6Sy"
 
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            for (var i = 0; i < numArticles; i++) {
+                console.log(response)
+                var articleURL = response.response.docs[i].web_url;
+                articleCounter ++;
+                var headlines = response.response.docs[i].headline.main;
+                var wellSection = $("<div>");
+                var a = $("<a>");
+                wellSection.attr("class", "counter");
+                wellSection.attr("data-article-" + articleCounter);
+                a.attr("href", articleURL)
+                a.text(headlines);
+                wellSection.append(a);
+                $("#articles").append(wellSection);
+            }
+        })
 
-
-
-
-
-
-
-
-
+    });
 
 });
